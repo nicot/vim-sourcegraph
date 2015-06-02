735ea77fc8da8daa3154834a8e23f9fbb95d3361
Nico Tonozzi <nico@nicot.us>
2015-06-02 14:49:25 -0700

    Pretty print json

--- plugin/sourcegraph.py
+++ plugin/sourcegraph.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 import vim
+import json
 
-def output(json, buf):
-    buf.append(json)
+def output(srcOutput, buf):
+    try:
+        js = json.loads(srcOutput)
+        s = json.dumps(js, indent=2)
+        for line in s.split('\n'):
+            buf.append(line)
+    except  ValueError:
+        buf.append(srcOutput)
     del buf[0]
 
-json = vim.eval("a:content")
+srcOutput = vim.eval("a:content")
 buf = vim.current.buffer
-output(json, buf)
+output(srcOutput, buf)
