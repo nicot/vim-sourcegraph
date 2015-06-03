9598b7a583c2d7d65f0a8b7c356cf104e7751d70
Nico Tonozzi <nico@nicot.us>
2015-06-02 17:11:12 -0700

    add source

--- plugin/sourcegraph.py
+++ plugin/sourcegraph.py
@@ -1,10 +1,42 @@
 #!/usr/bin/env python
 import vim
+import re
 import json
+from string import Template
+
+def definition(src):
+    t = Template('''\
+ Name: $name
+ Type: $Type
+ Kind: $kind
+ Import Path: $packageImportPath
+ File: $File
+
+ Source:
+ $source
+
+ ''')
+    s = t.substitute(name=src['Name'], Type=src['Data']['TypeString'],
+            kind=src['Kind'], packageImportPath=src['Data']['PackageImportPath'],
+            File=src['File'], source=readBuffer(src['File'], src['DefStart'], src['DefEnd']))
+    return s
+
+def readBuffer(path, start, stop):
+    with open(path) as f:
+        f.seek(start)
+        s = f.read(stop-start)
+        s = s.replace("\n", "\n ")
+        return s
 
 def output(srcOutput):
     try:
-        s = json.dumps(info, indent=4)
+        info = json.loads(srcOutput)
+        s = ""
+        if 'Def' in info:
+            s = definition(info['Def'])
+            s += json.dumps(info, indent=4)
+        else:
+            s = json.dumps(info, indent=4)
     except  ValueError:
         s = srcOutput
     return s
@@ -14,7 +46,7 @@ def write(string, buf):
         buf.append(line)
     del buf[0]
 
-srcOutput = vim.eval("a:content")
+srcOutput = vim.eval('a:content')
 buf = vim.current.buffer
 string = output(srcOutput)
 write(string, buf)
